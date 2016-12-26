9eb59b8ee10c029dde92a8d486de9107faa6e6da
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2016-12-26 12:14:20 -0500

    Updated feed-ui

--- sirGillSocial/CircleView.swift
+++ sirGillSocial/CircleView.swift
@@ -1,15 +1,15 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  sirGillSocial
 //
-//  Created by Surinder Singh Gill on 12/25/16.
+//  Created by Surinder Singh Gill on 12/26/16.
 //  Copyright © 2016 Surinder Singh Gill. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
-
+class CircleView: UIImageView {
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -17,7 +17,15 @@ class FancyView: UIView {
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+    }
+
+    override func draw(_ rect: CGRect){
+        
+        super.draw(rect)
+        
+        layer.cornerRadius = self.frame.width / 2
+    
+    
     }
 
 }
