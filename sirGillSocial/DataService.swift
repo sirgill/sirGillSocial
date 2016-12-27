c6a2a5679de41a72ce74f333d51428d9b0019009
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2016-12-27 12:59:51 -0500

    Firebase Like Btn completed

--- sirGillSocial/DataService.swift
+++ sirGillSocial/DataService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Firebase
+import SwiftKeychainWrapper
 
 let DB_BASE = FIRDatabase.database().reference()
 let STORAGE_BASE = FIRStorage.storage().reference()
@@ -34,9 +35,17 @@ class DataService {
         return _REF_USERS
     }
     
+    var REF_USERS_CURRENT: FIRDatabaseReference {
+        let uid = KeychainWrapper.standard.string(forKey: KEY_UID)
+        let user = REF_USERS.child(uid!)
+        return user
+    }
+    
     var REF_POST_IMAGES: FIRStorageReference {
         return _REF_POST_IMAGES
     }
+    
+    
     
     func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
         REF_USERS.child(uid).updateChildValues(userData)
--- sirGillSocial/FeedVC.swift
+++ sirGillSocial/FeedVC.swift
@@ -18,6 +18,10 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     
     @IBOutlet weak var captionField: FancyField!
     
+    @IBOutlet weak var likeImg: UIImageView!
+    
+    
+    
     var posts = [Post]()
     var imagePicker: UIImagePickerController!
     static var imageCache: NSCache<NSString, UIImage> = NSCache()
@@ -70,11 +74,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
             
             if let img = FeedVC.imageCache.object(forKey: post.imageUrl as NSString) {
                 cell.configureCell(post: post, img: img)
-                return cell
+                
             } else {
                 cell.configureCell(post: post)
-                return cell
+                
             }
+            return cell
             } else {
             return PostCell()
         }
@@ -156,7 +161,7 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIIm
     
     
     @IBAction func signOutTapped(_ sender: AnyObject) {
-        let keychainResult = KeychainWrapper.standard.remove(key:KEY_UID)
+        let keychainResult = KeychainWrapper.standard.removeObject(forKey:KEY_UID)
         print("Gill: Id removed form keychain \(keychainResult)")
         try! FIRAuth.auth()?.signOut()
         performSegue(withIdentifier:  "goToSignIn", sender: nil)
