0e6b2c854ee93a82baab2291f2dea8cf3db88356
sirgill <sunnygill@Surinders-MacBook-Pro.local>
2016-12-26 21:24:48 -0500

    Firebase Storage Image Cache

--- sirGillSocial/DataService.swift
+++ sirGillSocial/DataService.swift
@@ -10,6 +10,7 @@ import Foundation
 import Firebase
 
 let DB_BASE = FIRDatabase.database().reference()
+let STORAGE_BASE = FIRStorage.storage().reference()
 
 
 class DataService {
@@ -20,6 +21,8 @@ class DataService {
     private var _REF_POSTS = DB_BASE.child("post")
     private var _REF_USERS = DB_BASE.child("users")
     
+    private var _REF_POST_IMAGES = STORAGE_BASE.child("post-pics")
+    
     var REF_BASE: FIRDatabaseReference {
         return _REF_BASE
     }
@@ -29,6 +32,10 @@ class DataService {
     
     var REF_USERS: FIRDatabaseReference {
         return _REF_USERS
+    }
+    
+    var REF_POST_IMAGES: FIRStorageReference {
+        return _REF_POST_IMAGES
     }
     
     func createFirebaseDBUser(uid: String, userData: Dictionary<String, String>) {
